84331b038db2bc41f41e4e5002aa6cc0346d1540
Bob Kozdemba <bkozdemba@gmail.com>
2021-01-24 21:50:30 -0600

    Updated.

--- run-couchbase-server.sh
+++ run-couchbase-server.sh
@@ -2,5 +2,7 @@
 #
 # https://hub.docker.com/_/couchbase
 #
+# Connect to port 8091 with a web browser to configure the cluster. Install the 
+# travel-sample bucket to run the test client programs.
 
 podman run -d --name=couchbase -p 8091-8094:8091-8094 -p 11210-11211:11210-11211 couchbase
